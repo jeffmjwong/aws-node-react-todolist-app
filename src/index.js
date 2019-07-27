0df761ddef698d6ae9bafe540b400dd6346406fe
Jeff Wong <jeffmjwong@gmail.com>
2019-07-27 21:42:25 +1000

    Add index.css stylesheets and import from index.js

--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
+import './index.css';
+
 const newParagraph = document.createElement('p');
 newParagraph.innerHTML = 'This is from js file.';
+newParagraph.classList.add('cyan-background');
 
 document.body.appendChild(newParagraph);
