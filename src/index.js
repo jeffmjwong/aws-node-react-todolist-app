1f0a901f19c6da68943e8b7137a2780eaeefddc0
Jeff Wong <jeffmjwong@gmail.com>
2019-07-28 11:32:38 +1000

    Use styles in index.js entry file

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
-import './index.css';
+import './index.scss';
 
 const newParagraph = document.createElement('p');
 newParagraph.innerHTML = 'This is from js file.';
-newParagraph.classList.add('cyan-background');
+newParagraph.classList.add('background-main');
+newParagraph.classList.add('ml2');
 
 document.body.appendChild(newParagraph);
