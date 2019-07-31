2c2e35f8806f23934439f6cac87ab6b53f584fc7
Jeff Wong <jeffmjwong@gmail.com>
2019-07-31 19:30:37 +1000

    Create list to display item

--- src/components/App.js
+++ src/components/App.js
@@ -5,11 +5,25 @@ const App = () => {
 
   return (
     <div>
-      <label className="m1">
-        Add item to list:
-        <input />
-        <button>Add!</button>
-      </label>
+      <form>
+        <label className="m1">
+          Add item to list:
+
+          <input
+            className="ml05 mr05"
+          />
+
+          <button>Add!</button>
+        </label>
+      </form>
+
+      <ul>
+        {
+          itemList.map(item =>
+            <li>{item}</li>
+          )
+        }
+      </ul>
     </div>
   );
 };
