104ed3350b2f3b9d572385fc7a14d3a86a0f1082
Jeff Wong <jeffmjwong@gmail.com>
2019-08-02 21:23:22 +1000

    Get record from database and send it through routes

--- server/index.js
+++ server/index.js
@@ -18,7 +18,14 @@ const config = {
 const db = pgp(config);
 
 app.get('/', (req, res) => {
-  res.send('Hello world from Express!');
+  db.one('SELECT * from todos')
+    .then(dbres => {
+      console.log(dbres.name);
+      // res.send('Hello world from Postgresql!');
+      res.status(200).send(dbres.name);
+    })
+    .catch(err => console.log(err));
+  // res.send('Hello world from Express!');
 });
 
 app.listen(3001);
