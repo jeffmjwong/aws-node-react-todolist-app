29d70d435a2b42b28437f6de0a75b6b20fd8c350
Jeff Wong <jeffmjwong@gmail.com>
2019-08-02 21:09:25 +1000

    Connect to postgresql db in server index.js

--- server/index.js
+++ server/index.js
@@ -1,8 +1,21 @@
 require('dotenv').config();
 
 const express = require('express');
+const pgPromise = require('pg-promise');
 
 const app = express();
+
+const pgp = pgPromise({});
+
+const config = {
+  host: process.env.POSTGRES_DB_HOST,
+  port: process.env.POSTGRES_DB_PORT,
+  database: process.env.POSTGRES_DB_DATABASE,
+  user: process.env.POSTGRES_DB_USER,
+  password: process.env.POSTGRES_DB_PASSWORD,
+}
+
+const db = pgp(config);
 
 app.get('/', (req, res) => {
   res.send('Hello world from Express!');
