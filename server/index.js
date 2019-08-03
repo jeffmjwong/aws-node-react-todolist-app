1d95bfa92a3c625dcb680bfe49bb7531b573febd
Jeff Wong <jeffmjwong@gmail.com>
2019-08-03 14:43:52 +1000

    Clean up server index.js

--- server/index.js
+++ server/index.js
@@ -1,21 +1,8 @@
-require('dotenv').config();
-
-const express = require('express');
-const pgPromise = require('pg-promise');
+import './env';
+import express from 'express';
+import db from './db';
 
 const app = express();
-
-const pgp = pgPromise({});
-
-const config = {
-  host: process.env.POSTGRES_DB_HOST,
-  port: process.env.POSTGRES_DB_PORT,
-  database: process.env.POSTGRES_DB_DATABASE,
-  user: process.env.POSTGRES_DB_USER,
-  password: process.env.POSTGRES_DB_PASSWORD,
-}
-
-const db = pgp(config);
 
 app.get('/', (req, res) => {
   db.one('SELECT * from todos')
