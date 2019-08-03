371903cc8ff928f707d6febd95117b56b1692c60
Jeff Wong <jeffmjwong@gmail.com>
2019-08-03 15:54:10 +1000

    Update server index.js file

--- server/index.js
+++ server/index.js
@@ -1,10 +1,13 @@
-import './env';
 import express from 'express';
 import db from './db';
 
 const app = express();
 
 app.get('/', (req, res) => {
+  res.status(200).send('Hello world from Express!');
+});
+
+app.get('/todos', (req, res) => {
   db.one('SELECT * from todos')
     .then(dbres => {
       console.log(dbres.name);
@@ -15,4 +18,6 @@ app.get('/', (req, res) => {
   // res.send('Hello world from Express!');
 });
 
-app.listen(3001);
+app.listen(3001, () => {
+  console.log('Server has been started and listening on port 3001...')
+});
