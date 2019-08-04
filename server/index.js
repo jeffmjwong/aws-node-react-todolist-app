6425ccfeddcad0840287b6ba5206736bb71aeb71
Jeff Wong <jeffmjwong@gmail.com>
2019-08-04 17:22:04 +1000

    Use bodyParser in server index.js file

--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import cors from 'cors';
+import bodyParser from 'body-parser';
 import db from './db';
 
 const app = express();
@@ -9,6 +10,8 @@ const corsOptions = {
 };
 
 app.use(cors(corsOptions));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
   res.status(200).send('Hello world from Express!');
@@ -19,6 +22,11 @@ app.get('/todos', (req, res) => {
     .then(data => res.json(data))
     .catch(err => console.log(err));
 });
+
+app.post('/todos', (req, res) => {
+  debugger;
+  console.log(req);
+})
 
 app.listen(3001, () => {
   console.log('Server has been started and listening on port 3001...')
