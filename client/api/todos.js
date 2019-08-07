dfd0b63676613a7de53098c84a60bfd019c3ac44
Jeff Wong <jeffmjwong@gmail.com>
2019-08-07 20:07:27 +1000

    Create todos.js in api folder to handle todos related api

---
+++ client/api/todos.js
@@ -0,0 +1,13 @@
+import { API_URL } from './constants';
+
+export const getTodos = async () => {
+  const results = await fetch(`${API_URL}/todos`);
+
+  if (results.status === 422) {
+    const error = await results.json();
+    throw new Error(error);
+  } else {
+    const data = await results.json();
+    return data;
+  }
+};
