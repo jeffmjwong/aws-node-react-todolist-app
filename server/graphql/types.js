f6642b4224718e2bbab42d9113ab50b91a917cca
Jeff Wong <jeffmjwong@gmail.com>
2019-08-08 20:28:12 +1000

    Create TodoType in types.js

--- server/graphql/types.js
+++ server/graphql/types.js
@@ -1,2 +1,14 @@
 import { GraphQLObjectType, GraphQLString } from 'graphql';
 
+export const TodoType = new GraphQLObjectType({
+  name: 'Todo',
+  type: 'Query',
+  fields: {
+    id: { type: GraphQLString },
+    name: { type: GraphQLString },
+    completed: { type: GraphQLString },
+    number: { type: GraphQLString },
+    created_at: { type: GraphQLString },
+    updated_at: { type: GraphQLString }
+  }
+});
