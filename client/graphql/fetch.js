78a01b941eb7aa3cd02a2956199ad17e9f3c633a
Jeff Wong <jeffmjwong@gmail.com>
2019-08-10 21:47:51 +1000

    Create fetch.js to handle generate fetch request in graphql folder

---
+++ client/graphql/fetch.js
@@ -0,0 +1,18 @@
+import { GRAPHQL_URL } from './constants';
+import getTodosQuery from './queries/getTodos.gql';
+
+export const getTodosGraphQL = async () => {
+  const response = await fetch(GRAPHQL_URL, {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({ query: getTodosQuery })
+  });
+
+  if (response) {
+    const result = await response.json();
+    return result.data;
+  } else {
+    const result = await response.json();
+    throw new Error(result.data);
+  }
+};
