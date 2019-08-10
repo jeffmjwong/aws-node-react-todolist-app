3fabc9d91a4c088d505366bcccdcb7262fb066a1
Jeff Wong <jeffmjwong@gmail.com>
2019-08-10 17:24:51 +1000

    Build a test query type

--- server/graphql/queries.js
+++ server/graphql/queries.js
@@ -1,11 +1,21 @@
-import { GraphQLObjectType } from 'graphql';
-
+import { GraphQLObjectType, GraphQLString } from 'graphql';
 import { TodoType } from './queries';
+import db from '../db';
 
 export default new GraphQLObjectType({
-  name: 'RootQueryType',
+  name: 'RootQuery',
   type: 'Query',
   fields: {
-    todos: [TodoType]
+    user: {
+      type: GraphQLString,
+      args: { name: { type: GraphQLString } },
+      resolve(parentValue, args) {
+        if (args.name) {
+          return `Hello I am a user, my name is ${args.name}!`;
+        } else {
+          return 'Hello I am a user!';
+        }
+      }
+    }
   }
 })
